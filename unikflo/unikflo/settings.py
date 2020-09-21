d749bda5efafec5c8b889f41cfd8e669586379f6
aakashg1999 <37487198+aakashg1999@users.noreply.github.com>
2020-09-21 21:45:35 +0530

    Update settings.py
    Debug Off

--- unikflo/unikflo/settings.py
+++ unikflo/unikflo/settings.py
@@ -25,7 +25,7 @@ MEDIA_DIR=os.path.join(BASE_DIR,'media')
 SECRET_KEY = 'vjt&95^68pw+-dt$$(vvn)saupyo#p6cn=!zgz=p-!icd(h3%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
